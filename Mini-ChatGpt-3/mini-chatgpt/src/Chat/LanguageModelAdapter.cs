7c275763403e960959e7916fc4ea39442cde06b3
Zhovtiak Maksym <maxon.zhovtyak@gmail.com>
2026-04-10 02:35:59 +0300

    Added LanguageModelAdapter to support neural networks.

---
+++ Mini-ChatGpt-3/mini-chatgpt/src/Chat/LanguageModelAdapter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using MiniChatGPT.Contracts;
+using Lib.Models.TinyNN;
+using Lib.Models.TinyTransformer;
+
+namespace MiniChatGPT.Chat
+{
+    public class LanguageModelAdapter : ILanguageModel
+    {
+        private readonly object _originalModel;
+        private readonly string _modelKind;
+        private readonly int _vocabSize;
+
+        public LanguageModelAdapter(object originalModel, string modelKind, int vocabSize)
+        {
+            _originalModel = originalModel;
+            _modelKind = modelKind;
+            _vocabSize = vocabSize;
+        }
+
+        public string ModelKind
+        {
+            get
+            {
+                return _modelKind;
+            }
+        }
+
+        public int VocabSize
+        {
+            get
+            {
+                return _vocabSize;
+            }
+        }
+
+        public float[] NextTokenScores(ReadOnlySpan<int> context)
+        {
+            if (_modelKind == "tinynn")
+            {
+                TinyNNModel nn = (TinyNNModel)_originalModel;
+
+                return nn.NextTokenScores(context);
+            }
+            else
+            {
+                if (_modelKind == "tinytransformer")
+                {
+                    TinyTransformerModel tf = (TinyTransformerModel)_originalModel;
+
+                    return tf.NextTokenScores(context.ToArray());
+                }
+                else
+                {
+                    throw new InvalidOperationException("Невідомий тип моделі");
+                }
+            }
+        }
+
+        public object GetPayloadForCheckpoint()
+        {
+            if (_modelKind == "tinynn")
+            {
+                TinyNNModel nn = (TinyNNModel)_originalModel;
+
+                return nn.ToPayload();
+            }
+            else
+            {
+                if (_modelKind == "tinytransformer")
+                {
+                    TinyTransformerModel tf = (TinyTransformerModel)_originalModel;
+
+                    return tf.GetPayloadForCheckpoint();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public string GetContractFingerprint()
+        {
+            return "LanguageModelAdapter-Integration";
+        }
+    }
+}
--- Mini-ChatGpt-3/mini-chatgpt/src/Chat/Program.cs
+++ Mini-ChatGpt-3/mini-chatgpt/src/Chat/Program.cs
@@ -2,11 +2,13 @@
 using System.Text.Json;
 using ContractTokenizer = MiniChatGPT.Contracts.ITokenizer;
 using MiniChatGPT.Contracts;
-using Lib.MathCore; 
+using Lib.MathCore;
 using Lib.Tokenization;
 using Lib.Runtime;
 using NGram.ModelFactory;
 using MiniChatGPT.ChatConsole;
+using Lib.Models.TinyNN.Factories;
+using Lib.Models.TinyTransformer.Factories;
 
 namespace MiniChatGPT.Chat
 {
@@ -17,32 +19,44 @@ namespace MiniChatGPT.Chat
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
 
-            string fileName = "checkpoint.json";
             string checkpointPath = "";
             float temp = 0.3f;
             int topK = 5;
 
-            string[] possiblePaths =
+            for (int i = 0; i < args.Length; i++)
             {
-                Path.Combine(Environment.CurrentDirectory, "data", fileName),
-                Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "data", fileName)),
-                Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "data", fileName))
-            };
-
-            foreach (string path in possiblePaths)
-            {
-                if (File.Exists(path))
+                if (args[i] == "--checkpoint")
                 {
-                    checkpointPath = path;
+                    if (i + 1 < args.Length)
+                    {
+                        checkpointPath = args[i + 1];
+                    }
+                }
+            }
 
-                    break;
+            if (string.IsNullOrEmpty(checkpointPath))
+            {
+                string fileName = "checkpoint.json";
+                string[] possiblePaths =
+                {
+                    Path.Combine(Environment.CurrentDirectory, "data", fileName),
+                    Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "data", fileName)),
+                    Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "data", fileName))
+                };
+
+                foreach (string path in possiblePaths)
+                {
+                    if (File.Exists(path))
+                    {
+                        checkpointPath = path;
+                        break;
+                    }
                 }
             }
 
             if (string.IsNullOrEmpty(checkpointPath))
             {
                 Console.WriteLine("Не знайдено файл конфігурації.");
-
                 return 1;
             }
 
@@ -52,14 +66,50 @@ namespace MiniChatGPT.Chat
                 Checkpoint payload = io.Load(checkpointPath);
 
                 JsonElement tokJson = (JsonElement)payload.TokenizerPayload;
-                object rawTokenizer = payload.TokenizerKind == "word"? new WordTokenizerFactory().FromPayload(tokJson) : new CharTokenizerFactory().FromPayload(tokJson);
+                object rawTokenizer;
+
+                if (payload.TokenizerKind == "word")
+                {
+                    rawTokenizer = new WordTokenizerFactory().FromPayload(tokJson);
+                }
+                else
+                {
+                    rawTokenizer = new CharTokenizerFactory().FromPayload(tokJson);
+                }
 
                 ContractTokenizer tokenizer = new TokenizerAdapter(rawTokenizer);
 
                 JsonElement modJson = (JsonElement)payload.ModelPayload;
-                ILanguageModel model = payload.ModelKind == "bigram"? new NGramModelFactory().CreateBigramModelFromPayload(tokenizer.VocabSize, modJson): new NGramModelFactory().CreateTrigramModelFromPayload(tokenizer.VocabSize, modJson);
-
+                ILanguageModel model = null;
+                bool requiresSoftmax = false;
                 IMathOps mathOps = new MathOpsImpl();
+
+                if (payload.ModelKind == "bigram")
+                {
+                    model = new NGramModelFactory().CreateBigramModelFromPayload(tokenizer.VocabSize, modJson);
+                }
+                else if (payload.ModelKind == "trigram")
+                {
+                    model = new NGramModelFactory().CreateTrigramModelFromPayload(tokenizer.VocabSize, modJson);
+                }
+                else if (payload.ModelKind == "tinynn")
+                {
+                    object rawTinyNN = new TinyNNModelFactory().FromPayload(modJson, tokenizer.VocabSize, mathOps);
+                    model = new LanguageModelAdapter(rawTinyNN, "tinynn", tokenizer.VocabSize);
+                    requiresSoftmax = true;
+                }
+                else if (payload.ModelKind == "tinytransformer")
+                {
+                    object rawTransformer = TinyTransformerModelFactory.FromPayload(modJson);
+                    model = new LanguageModelAdapter(rawTransformer, "tinytransformer", tokenizer.VocabSize);
+                    requiresSoftmax = true;
+                }
+                else
+                {
+                    Console.WriteLine("Невідомий тип моделі.");
+                    return 1;
+                }
+
                 MiniChatGPT.Sampling.Interfaces.ISampler sampler = new MiniChatGPT.Sampling.Sampler(mathOps);
 
                 RuntimeTextGenerator generator = new RuntimeTextGenerator(
@@ -67,7 +117,7 @@ namespace MiniChatGPT.Chat
                     sampler,
                     mathOps,
                     tokenizer,
-                    false
+                    requiresSoftmax
                 );
 
                 Console.WriteLine("\nСистема MiniChatGPT активована");
